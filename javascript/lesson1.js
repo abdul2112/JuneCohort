0961b057b9008bed2fdb87fb88d22c9d64010774
abdulkadir <abdulkadir.s.salad1@gmail.com>
2023-07-24 16:18:31 +0100

    changed folder names

---
+++ javascript/lesson1.js
@@ -0,0 +1,16 @@
+// This is a single line comment
+
+/*
+this
+will
+be
+a
+multi-line
+comment*/
+
+console.log('Hello you have executed the lesson 1 file');
+
+console.log('Hello World');
+
+var greeting = 'hello';
+var myName = 'abdulkadir';
